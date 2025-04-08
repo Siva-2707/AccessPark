eb1bdc283ac3372100abd3535e6733b5c421a30a
Sivarajesh Balamurali <b.sivarajesh20@gmail.com>
2025-04-08 16:34:35 -0300

    Initial Commit

---
+++ src/ingestor/halifax_ingestor.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import requests
+
+def get_halifax_data():
+  URL = 'https://services2.arcgis.com/11XBiaBYA9Ep0yNJ/arcgis/rest/services/Accessible_Parking_Spots/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
+  data = requests.get(URL).json()
+  # print(json.dumps(data,indent=4))
+  df = pd.DataFrame(data['features'])
+  property_df = pd.DataFrame([row['properties'] for row in data['features']])
+  coordinates_df = pd.DataFrame([row['geometry'] for row in data['features']])
+  df = pd.concat([property_df,coordinates_df],axis=1)
+  df.rename(columns={'ACCPRKID':'CITY_LOT_ID','STREET_NAME':'NAME','NUMSPOTS':'NO_OF_SPOTS','coordinates':'LOCATION'}, inplace=True)
+  df = df[['CITY_LOT_ID','NAME','NO_OF_SPOTS','LOCATION']]
+  df['CITY'] = 'Halifax'
+  df['STATE'] = 'NS'
+  df['COUNTRY'] = 'Canada'
+  return df
---
+++ src/ingestor/toronto_ingestor.py
@@ -0,0 +1,43 @@
+import requests
+import pandas as pd
+import ast
+
+def get_toronto_data():
+
+  # Toronto Open Data is stored in a CKAN instance. It's APIs are documented here:
+  # https://docs.ckan.org/en/latest/api/
+
+  # To hit our API, you'll be making requests to:
+  base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
+
+  # Datasets are called "packages". Each package can contain many "resources"
+  # To retrieve the metadata for this package and its resources, use the package name in this page's URL:
+  url = base_url + "/api/3/action/package_show"
+  params = { "id": "parking-lot-facilities"}
+  package = requests.get(url, params = params).json()
+
+  # To get resource data:
+  for idx, resource in enumerate(package["result"]["resources"]):
+        # To get metadata for non datastore_active resources:
+        if 'readme' not in resource['name']  and not resource["datastore_active"]:
+            url = base_url + "/api/3/action/resource_show?id=" + resource["id"]
+            resource_metadata = requests.get(url).json()
+            # print(json.dumps(resource_metadata,indent=4))
+            # From here, you can use the "url" attribute to download this file
+
+  toronto_data = pd.read_excel(resource_metadata['result']['url'])
+  print(f'Toronto data before filtering: {len(toronto_data)}')
+  if not toronto_data.empty:
+    toronto_data = toronto_data[(toronto_data['Access'].str.lower() == 'public') & (toronto_data['Handicap Parking Spaces'] > 0)]
+
+  toronto_data = toronto_data.rename(columns={'Parking Lot Asset ID':'CITY_LOT_ID','Park Name':'NAME','Handicap Parking Spaces':'NO_OF_SPOTS','GIS Coordinate':'LOCATION'})
+  toronto_data = toronto_data[['CITY_LOT_ID','NAME','NO_OF_SPOTS','LOCATION']]
+  toronto_data['LOCATION'] = toronto_data['LOCATION'].apply(convert_to_list)
+  toronto_data['CITY'] = 'Toronto'
+  toronto_data['STATE'] = 'ON'
+  toronto_data['COUNTRY'] = 'Canada'
+  print(f'Toronto data after filtering: {len(toronto_data)}')
+  return toronto_data
+
+def convert_to_list(coord_str):
+    return list(ast.literal_eval(coord_str))
